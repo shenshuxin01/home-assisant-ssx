9c27fe904f8cf1d2fa0cb4e183b0ddb37cbd88d0
shenshuxin01 <ssx17852015601@163.com>
2023-07-24 15:32:38 +0800

    警告面板添加其他字段信息6

--- custom_components/example_load_platform/text.py
+++ custom_components/example_load_platform/text.py
@@ -2,6 +2,7 @@
 from __future__ import annotations
 
 import time
+from datetime import timedelta
 
 from homeassistant.components.text import TextEntity
 from homeassistant.core import HomeAssistant
@@ -25,7 +26,7 @@ def setup_platform(
     # We only want this platform to be set up via discovery.
     if discovery_info is None:
         return
-    add_entities([DemoText()])
+    add_entities([DemoText(hass)], update_before_add=True)
 
 
 class DemoText(TextEntity):
@@ -33,7 +34,16 @@ class DemoText(TextEntity):
     # The value of the text.
     _attr_native_value = 'DemoText'
 
-    def __init__(self):
+    SCAN_INTERVAL = timedelta(seconds=5)
+
+    def __init__(self,hass):
+        """Initialize my coordinator."""
+        super().__init__(
+            hass,
+            _LOGGER,
+            # Polling interval. Will only be polled if there are subscribers.
+            update_interval=timedelta(seconds=10)
+        )
         #         _LOGGER.info(f'turn_on.kwargs={kwargs}')
         _LOGGER.info('init DemoText start!')
         self._attr_device_info = "ssx_DemoText_attr_device_info"  # For automatic device registration
