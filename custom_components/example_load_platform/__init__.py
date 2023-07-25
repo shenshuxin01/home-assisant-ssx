fad9c713cbc616ab84643db4ed7c76f0147cfba0
shenshuxin01 <ssx17852015601@163.com>
2023-07-25 09:25:26 +0800

    警告面板添加其他字段信息13

--- custom_components/example_load_platform/__init__.py
+++ custom_components/example_load_platform/__init__.py
@@ -21,7 +21,7 @@ def setup(hass: HomeAssistant, config: ConfigType) -> bool:
     hass.helpers.discovery.load_platform('lock', DOMAIN, {}, config)
     hass.helpers.discovery.load_platform('camera', DOMAIN, {}, config)
     # hass.helpers.discovery.load_platform('alarm_control_panel', DOMAIN, {}, config)
-    hass.helpers.discovery.load_platform(Platform.ALARM_CONTROL_PANEL, 'ssx_platform_acp1', {}, config)
+    hass.helpers.discovery.load_platform(Platform.ALARM_CONTROL_PANEL, DOMAIN, {}, config)
     hass.helpers.discovery.load_platform('text', DOMAIN, {}, config)
 
     return True
