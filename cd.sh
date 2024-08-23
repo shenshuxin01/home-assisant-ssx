8af263b2537f3c1a6d023c8b6166cbddceabeb62
shenshuxin <ssx17852015601@163.com>
2024-08-23 23:07:46 +0800

    部署node9

--- cd.sh
+++ cd.sh
@@ -1,7 +1,7 @@
 nd1_hass=/home/app/apps/k8s/for_docker_volume/homeassistant/config
 
-scp configuration.yaml secrets.yaml  root@node101:${nd1_hass} \
-  && ssh root@node101 "rm -rf ${nd1_hass}/custom_components/example_load_platform" \
-  && scp -r ./custom_components/example_load_platform  root@node101:${nd1_hass}/custom_components \
-  && ssh root@node101 "kubectl delete pod -n ssx \`kubectl get pod -n ssx | grep homeassistant | awk '{print \$1}'\`"
+scp configuration.yaml secrets.yaml  root@node109:${nd1_hass} \
+  && ssh root@node109 "rm -rf ${nd1_hass}/custom_components/example_load_platform" \
+  && scp -r ./custom_components/example_load_platform  root@node109:${nd1_hass}/custom_components \
+  && ssh root@node109 "kubectl delete pod -n ssx \`kubectl get pod -n ssx | grep homeassistant | awk '{print \$1}'\`"
 
--- custom_components/example_load_platform/ssx_utils.py
+++ custom_components/example_load_platform/ssx_utils.py
@@ -1,4 +1,3 @@
-
 import requests
 
 
@@ -24,7 +23,7 @@ def getDellR410Info() -> DellR410Info:
     # {'temperature': '30', 'speed': 8448.0}
     if resp.status_code != 200:
         raise IOError('请求dell风扇转速Python接口失败！！')
-    respJson = resp.json() #{"fanSpeed":"7920","temperature":32}
+    respJson = resp.json()  #{"fanSpeed":"7920","temperature":32}
     d = DellR410Info()
     d.temperature = respJson['temperature']
     d.speed = respJson['fanSpeed']
@@ -32,23 +31,31 @@ def getDellR410Info() -> DellR410Info:
 
 
 def getNode12CpuMemInfo() -> DellR410Node12CpuMemInfo:
-    resp = requests.get(
-        'http://192.168.0.101:5000/get_node12_cpu_mem_df_info')
-    # {
-    #     "cpu": "16.0",
-    #     "cpuDesc": " root 16.0 0.1 /usr/local/bin/ffmpeg -v info -t 77572 -i rtsp://admin:AGXXZI@192.168.0.105:554/h264/ch1/sub/av_stream -force_key_frames expr:gte(t,n_forced*1) -hls_time 60 -threads 1 -hls_list_size 0 -hls_segment_filename /ssxtmp/index_1690223228_%20d.ts /ssxtmp/index_1690223228.m3u8 \n",
-    #     "mem": "3.4",
-    #     "memDesc": " root 0.8 3.4 java -DuserLogbackLevel=error -Dloader.path=/home/app/apps/k8s/for_docker_volume/jenkins/jars/eureka-server -jar /myappjar/app.jar --spring.profiles.active=prod \n"
-    # }
-    if resp.status_code != 200:
-        raise IOError('请求node12内存处理器信息Python接口失败！！')
-    respJson = resp.json() #{"fanSpeed":"7920","temperature":32}
-    d2 = DellR410Node12CpuMemInfo()
-    d2.cpu = respJson['cpu']
-    d2.cpuDesc = respJson['cpuDesc']
-    d2.mem = respJson['mem']
-    d2.memDesc = respJson['memDesc']
-    return d2
+    if True:
+        d2 = DellR410Node12CpuMemInfo()
+        d2.cpu = 1
+        d2.cpuDesc = '11'
+        d2.mem = 2
+        d2.memDesc = '22'
+        return d2
+
+    # resp = requests.get(
+    #     'http://192.168.0.101:5000/get_node12_cpu_mem_df_info')
+    # # {
+    # #     "cpu": "16.0",
+    # #     "cpuDesc": " root 16.0 0.1 /usr/local/bin/ffmpeg -v info -t 77572 -i rtsp://admin:AGXXZI@192.168.0.105:554/h264/ch1/sub/av_stream -force_key_frames expr:gte(t,n_forced*1) -hls_time 60 -threads 1 -hls_list_size 0 -hls_segment_filename /ssxtmp/index_1690223228_%20d.ts /ssxtmp/index_1690223228.m3u8 \n",
+    # #     "mem": "3.4",
+    # #     "memDesc": " root 0.8 3.4 java -DuserLogbackLevel=error -Dloader.path=/home/app/apps/k8s/for_docker_volume/jenkins/jars/eureka-server -jar /myappjar/app.jar --spring.profiles.active=prod \n"
+    # # }
+    # if resp.status_code != 200:
+    #     raise IOError('请求node12内存处理器信息Python接口失败！！')
+    # respJson = resp.json()  #{"fanSpeed":"7920","temperature":32}
+    # d2 = DellR410Node12CpuMemInfo()
+    # d2.cpu = respJson['cpu']
+    # d2.cpuDesc = respJson['cpuDesc']
+    # d2.mem = respJson['mem']
+    # d2.memDesc = respJson['memDesc']
+    # return d2
 
 
 if __name__ == '__main__':
